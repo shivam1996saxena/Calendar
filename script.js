0f063c3811caabda60590b2636419b9d01767cbf
shivam1996saxena <shivamsaxena1996@outlook.com>
2023-01-31 04:14:14 +0530

    commit

--- script.js
+++ script.js
@@ -1,151 +1,93 @@
-var calendarNode = document.querySelector("#calendar");
-
-var currDate = new Date();
-var currYear = currDate.getFullYear();
-var currMonth = currDate.getMonth() + 1;
-
-var selectedYear = currYear;
-var selectedMonth = currMonth;
-var selectedMonthName = getMonthName(selectedYear, selectedMonth);
-var selectedMonthDays = getDayCount(selectedYear, selectedMonth);
-
-renderDOM(selectedYear, selectedMonth);
-
-function getMonthName (year, month) {
-    let selectedDate = new Date(year, month-1, 1);
-    return selectedDate.toLocaleString('default', { month: 'long' });
+const darkModeSwitch = document.querySelector('.dark-mode-switch');
+darkModeSwitch.onclick = () => {
+    document.querySelector('body').classList.toggle('dark');
+    document.querySelector('body').classList.toggle('light');
 }
 
-function getMonthText () {
-    if (selectedYear === currYear)
-        return selectedMonthName;
-    else
-        return selectedMonthName + ", " + selectedYear;
+// checking leap year
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 4 !== 0) ||  (year % 100 === 0 && year % 400 === 0)
 }
 
-function getDayName (year, month, day) {
-    let selectedDate = new Date(year, month-1, day);
-    return selectedDate.toLocaleDateString('en-US',{weekday: 'long'});
+
+getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
 }
 
-function getDayCount (year, month) {
-    return 32 - new Date(year, month-1, 32).getDate();
+let calendar = document.querySelector('.calendar');
+
+const monthNames = ['January','February','March','April','May','June','July','August','September', 'October','November','December',];
+
+let monthPicker = document.querySelector('#month-picker')
+
+monthPicker.onclick = () => {
+    monthList.classList.add('show');
 }
 
-function getDaysArray () {
-    let emptyFieldsCount = 0;
-    let emptyFields = [];
-    let days = [];
+// generate calendar
 
-    switch(getDayName(selectedYear, selectedMonth, 1))
-    {
-        case "Tuesday":
-            emptyFieldsCount = 1;
-            break;
-        case "Wednesday":
-            emptyFieldsCount = 2;
-            break;
-        case "Thursday":
-            emptyFieldsCount = 3;
-            break;
-        case "Friday":
-            emptyFieldsCount = 4;
-            break;
-        case "Saturday":
-            emptyFieldsCount = 5;
-            break;
-        case "Sunday":
-            emptyFieldsCount = 6;
-            break;
+generateCalendar = (month,year) => {
+    let calendarDays = document.querySelector('.calendar-days');
+    calendarDays.innerHTML = '';
+    let calendarHeaderYear = document.querySelector('#year');
+
+    let daysOfMonth = [31, getFebDays(year) , 31 , 30 , 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let currentDate = new Date();
+
+    monthPicker.innerHTML = monthNames[month];
+    calendarHeaderYear.innerHTML = year;
+
+    let firstDay = new Date(month, year, 1);
+
+    for (let i = 0; i <= daysOfMonth[month] + firstDay.getDay() - 1; i++) {
+        let day = document.createElement('div');
+        if (i >= firstDay.getDay()) {
+            day.classList.add('calendar-day-hover');
+            day.innerHTML = i - firstDay.getDay() + 1;
+            day.innerHTML += `<span> </span>
+                              <span> </span>
+                              <span> </span>
+                              <span> </span>
+                              <span> </span>`
+            if (i - firstDay.getDay() + 1 === currentDate.getDate() && year === currentDate.getFullYear() && month === currentDate.getMonth()) {
+                day.classList.add('curr-date')
+            }
+        }       
+        calendarDays.appendChild(day);
     }
-  
-    emptyFields = Array(emptyFieldsCount).fill("");
-    days = Array.from(Array(selectedMonthDays + 1).keys());
-    days.splice(0, 1);
-    
-    return emptyFields.concat(days);
 }
 
-function renderDOM (year, month) {
-  let newCalendarNode = document.createElement("div");
-  newCalendarNode.id = "calendar";
-  newCalendarNode.className = "calendar";
-  
-  let dateText = document.createElement("div");
-  dateText.append(getMonthText());
-  dateText.className = "date-text";
-  newCalendarNode.append(dateText);
-  
-  let leftArrow = document.createElement("div");
-  leftArrow.append("«");
-  leftArrow.className = "button";
-  leftArrow.addEventListener("click", goToPrevMonth);
-  newCalendarNode.append(leftArrow);
-  
-  let curr = document.createElement("div");
-  curr.append("📅");
-  curr.className = "button";
-  curr.addEventListener("click", goToCurrDate);
-  newCalendarNode.append(curr);
-  
-  let rightArrow = document.createElement("div");
-  rightArrow.append("»");
-  rightArrow.className = "button";
-  rightArrow.addEventListener("click", goToNextMonth);
-  newCalendarNode.append(rightArrow);
-  
-  let dayNames = ["M", "T", "W", "T", "F", "S", "S"];
-  
-  dayNames.forEach((cellText) => {
-    let cellNode = document.createElement("div");
-    cellNode.className = "cell cell--unselectable";
-    cellNode.append(cellText);
-    newCalendarNode.append(cellNode);
-  });
-  
-  let days = getDaysArray(year, month);
-  
-  days.forEach((cellText) => {
-    let cellNode = document.createElement("div");
-    cellNode.className = "cell";
-    cellNode.append(cellText);
-    newCalendarNode.append(cellNode);
-  });
-  
-  calendarNode.replaceWith(newCalendarNode);
-  calendarNode = document.querySelector("#calendar");
-}
+let monthList = calendar.querySelector('.month-list');
 
-function goToPrevMonth () {
-    selectedMonth--;
-    if (selectedMonth === 0) {
-        selectedMonth = 12;
-        selectedYear--;
+monthNames.forEach((e,i) => {
+    let month = document.createElement('div');
+    month.innerHTML = `<div>${e}</div>`;
+    month.onclick = () => {
+        monthList.classList.remove('show');
+        currMonth.value = i;
+        generateCalendar(currMonth.value,currYear.value)
     }
-    selectedMonthDays = getDayCount(selectedYear, selectedMonth);
-    selectedMonthName = getMonthName(selectedYear, selectedMonth);
-  
-    renderDOM(selectedYear, selectedMonth);
+    monthList.appendChild(month);
+})
+
+const prevYear = document.querySelector('#prev-year');
+prevYear.onclick = () => {
+    --currYear.value;
+    generateCalendar(currMonth.value,currYear.value);
 }
 
-function goToNextMonth () {
-    selectedMonth++;
-    if (selectedMonth === 13) {
-        selectedMonth = 0;
-        selectedYear++;
-    }
-    selectedMonthDays = getDayCount(selectedYear, selectedMonth);
-    selectedMonthName = getMonthName(selectedYear, selectedMonth);
-  
-    renderDOM(selectedYear, selectedMonth);
+const nextYear = document.querySelector('#next-year');
+nextYear.onclick = () => {
+    ++currYear.value;
+    generateCalendar(currMonth.value,currYear.value )
 }
+let currDate = new Date();
+console.log(currDate);
 
-function goToCurrDate () {
-    selectedYear = currYear;
-    selectedMonth = currMonth;
+let currMonth = {value: currDate.getMonth()};
+console.log(currMonth.value);
 
-    selectedMonthDays = getDayCount(selectedYear, selectedMonth);
-    selectedMonthName = getMonthName(selectedYear, selectedMonth);
-  
-    renderDOM(selectedYear, selectedMonth);
-}
+let currYear = {value : currDate.getFullYear()};
+
+generateCalendar(currMonth.value, currYear.value)
